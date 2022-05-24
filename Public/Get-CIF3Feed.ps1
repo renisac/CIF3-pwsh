0e1b6fbe375504496b280e84a429ff34dc73a559
Michael Davis <mdavis332@gmail.com>
2022-05-24 09:44:19 -0500

    specific examples on `-ExtraParams`

--- Public/Get-CIF3Feed.ps1
+++ Public/Get-CIF3Feed.ps1
@@ -21,7 +21,7 @@ function Get-CIF3Feed {
         # Return feed of high-confidence md5 hashes tagged as 'malware,' 
         # and include additional param in API call which will be added to URL parameter as "?testKey=testValue"
         # This enables passing URL parameters supported by the REST API that may not have explicit params supported by this module.
-        PS C:\> Get-CIF3Feed -Confidence 8 -Tag malware -IType md5 -ExtraParams @{ 'testKey' = 'testValue' }
+        PS C:\> Get-CIF3Feed -Confidence 8 -Tag malware -IType md5 -ExtraParams @{ 'tlp' = 'green' }
 
     .OUTPUTS
         A an array of PSCustomObjects from CIF instance's API composed of indicator properties.
